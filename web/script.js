6a1bfdad86dc2b6de707b8bfeea21a84e443f504
xanonDev <52205282+xanonDev@users.noreply.github.com>
2023-08-05 14:25:10 +0200

    Update script.js

--- web/script.js
+++ web/script.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         const elements = document.querySelectorAll('[style*="display: none;"]');
         let delay = 0;
+        const skipbtn = document.getElementById("skip");
+        scrollYN = true;
+
+        skipbtn.addEventListener('click', function() {
+            scrollYN = false;
+            elements.forEach(element => {
+                element.style.display = "block";
+            });
+        });
 
         function getRandomArbitrary(min, max) {
             return Math.random() * (max - min) + min;
@@ -10,7 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
         elements.forEach(element => {
             setTimeout(() => {
                 element.style.display = "block";
-                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                if (scrollYN === true) {
+                    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                }
             }, delay);
             delay += getRandomArbitrary(300, 1000);
         });
