98d55d2ed50a500a6170ea6f104bf12687f29dd0
Jvr <109031036+Jvr2022@users.noreply.github.com>
2023-08-20 19:05:28 +0200

    ask to start installation

--- setup.bat
+++ setup.bat
@@ -1,3 +1,10 @@
-python setup.py
-py setup.py
+set /p install=Do you want to start the Pinkcord installation? (yes/no): 
+
+if /i "%install%"=="yes" (
+    python setup.py
+    py setup.py
+) else (
+    echo Installation canceled.
+)
+
 pause
