f431e5c6625203ea9fc8f9316f015af927cea192
Jvr <109031036+Jvr2022@users.noreply.github.com>
2023-08-21 18:07:32 +0200

    File rename

--- setup/Setup.bat
+++ setup/Setup.bat
@@ -2,8 +2,8 @@
 set /p install=Do you want to start the Pinkcord installation? (yes/no): 
 
 if /i "%install%"=="yes" (
-    python setup.py
-    py setup.py
+    python Setup.py
+    py Setup.py
 ) else (
     echo Installation canceled.
 )
