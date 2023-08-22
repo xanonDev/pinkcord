087a62eb85def3727385b894c7453aecd5e30121
xanonDev <52205282+xanonDev@users.noreply.github.com>
2023-08-22 14:43:54 +0200

    Create Setup.bat

---
+++ src/Setup.bat
@@ -0,0 +1,11 @@
+@echo off 
+set /p install=Do you want to start the Pinkcord installation? (yes/no): 
+
+if /i "%install%"=="yes" (
+    python Setup.py
+    py Setup.py
+) else (
+    echo Installation canceled.
+)
+
+pause
