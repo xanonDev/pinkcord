e89a29a47391e1ad641ff5add7c391a52f94bc62
Jvr <109031036+Jvr2022@users.noreply.github.com>
2023-08-28 10:54:47 +0200

    Fix bug

--- src/Setup.bat
+++ src/Setup.bat
@@ -7,8 +7,11 @@ if %errorlevel% NEQ 0 (
     if /i "%install_python%"=="yes" (
         REM Download and install Python
         REM The following lines download the installer and execute it (for Windows only)
-        curl -o python_installer.exe https://www.python.org/ftp/python/3.11.5/python-3.11.5-embed-amd64.zip
+        bitsadmin /transfer "PythonInstaller" https://www.python.org/ftp/python/3.11.5/python-3.11.5-embed-amd64.zip %CD%\python_installer.zip
+        tar -xf python_installer.zip
+        cd Python-3.11.5-embed-amd64
         python_installer.exe
+        cd ..
         REM Add code here to install Python on other platforms
     ) else (
         echo Python installation canceled.
