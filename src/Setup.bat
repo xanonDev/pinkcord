4eed72510ef91945cdba1c90d3f98b081246ed50
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-05-17 10:15:28 +0200

    use pinkcord 2.0

--- src/Setup.bat
+++ src/Setup.bat
@@ -1,5 +1,5 @@
 @echo off 
-set /p install=Do you want to start the Pinkcord installation? (yes/no): 
+set /p install=Do you want to start the Pinkcord 2.0 installation? (yes/no): 
 
 if /i "%install%"=="yes" (
     python Setup.py || (
